dc61c671d48c64d9a778e87c8e9a500dba9816fe
KaoWebDesign <assdwqe@gmail.com>
2019-08-10 22:59:40 +0300

    upd menu

--- static/js/common.js
+++ static/js/common.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     let burgerMenu = $('#burgerMenu'),
         menu = $('#menu'),
+        menuNav = $('.mainNav'),
         menuWrap = menu.find('.menu__wrap');
 
     burgerMenu.on('click', function() {
         menu.toggleClass('menu_active');
         menuWrap.toggleClass('menu__wrap_active');
+        menuNav.toggleClass('mainNav_active');
+        burgerMenu.toggleClass('burgerMenu_active');
     });
 });
